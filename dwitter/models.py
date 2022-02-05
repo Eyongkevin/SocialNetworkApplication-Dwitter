fe845012ccb57b4f87765c3464359508c41c96ea
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2022-02-05 21:35:35 +0100

    set signal for auto profile creation with test

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -13,3 +15,12 @@ class Profile(models.Model):
     )
     def __str__(self) -> str:
         return self.user.username
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(user_profile)
+
+post_save.connect(create_profile, sender=User)
--- dwitter/tests/test_models.py
+++ dwitter/tests/test_models.py
@@ -5,20 +5,12 @@ from django.contrib.auth.models import User
 
 class ProfileModelTestCase(TestCase):
     def setUp(self) -> None:
-        self.user_kevin = User.objects.create(username='kevin')
-        self.user_tassimo = User.objects.create(username='tassimo')
-
-        self.tassimo_profile = Profile.objects.create(user=self.user_tassimo)
-        self.kevin_profile = Profile.objects.create(user=self.user_kevin)
-
-        self.kevin_profile.follows.add(self.tassimo_profile)
-
+        User.objects.create(username='kevin')
     
     def test_profile_created(self):
-        self.assertEqual(self.kevin_profile.user.username, 'kevin')
+        self.assertTrue(Profile.objects.filter(user__username= 'kevin').exists())
     
-    def test_profile_has_followers(self):
-        self.assertIn(self.tassimo_profile, self.kevin_profile.follows.all())
+    def test_profile_follows_self(self):
+        self.kevin_profile = Profile.objects.get(user__username= 'kevin')
 
-    def test_profile_no_followers(self):
-        self.assertFalse(self.tassimo_profile.follows.all().exists())
+        self.assertIn(self.kevin_profile, self.kevin_profile.follows.all())
