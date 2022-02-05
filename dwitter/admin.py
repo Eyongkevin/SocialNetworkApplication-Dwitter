0dd74c96149986dda9983083ad9b45d4e8d9a682
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2022-02-05 18:44:49 +0100

    customize User model

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,5 +1,14 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 from django.contrib import admin
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ['username']
+
+
+# Unregister your models here
 admin.site.unregister(Group)
+admin.site.unregister(User)
+
+# Register your models here.
+admin.site.register(User, UserAdmin)
