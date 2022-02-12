727752e20000c0e0a5a4038cfaa3ecc967428cdf
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2022-02-12 10:17:50 +0100

    use decorators for signals

--- dwitter/models.py
+++ dwitter/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -16,11 +17,10 @@ class Profile(models.Model):
     def __str__(self) -> str:
         return self.user.username
 
-
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
         user_profile.save()
         user_profile.follows.add(user_profile)
-
-post_save.connect(create_profile, sender=User)
+        user_profile.save()
