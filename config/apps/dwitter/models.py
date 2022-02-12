93946696d539590d268b32d230b6d96909543dfe
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2022-02-12 10:49:34 +0100

    separate signal in its own file

--- config/apps/dwitter/models.py
+++ config/apps/dwitter/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 # Create your models here.
 
@@ -17,10 +15,4 @@ class Profile(models.Model):
     def __str__(self) -> str:
         return self.user.username
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        user_profile = Profile(user=instance)
-        user_profile.save()
-        user_profile.follows.add(user_profile)
-        user_profile.save()
+
---
+++ config/apps/dwitter/signals.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Profile
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(user_profile)
+        user_profile.save()
